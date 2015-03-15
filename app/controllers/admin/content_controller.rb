03ce58505b92bdc3adebf47a3ddb28f95a52fd6e
lamckenneth <lam.c.kenneth@berkeley.edu>
2015-03-15 15:39:24 -0700

    stuff

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -39,7 +39,7 @@ class Admin::ContentController < Admin::BaseController
       end
       @article.save!
       @merge.destroy
-      redirect_to admin_content_path(:edit, @article.id) #somewhere
+      redirect_to admin_content_edit_path(@article.id) #somewhere
     end
     @article = Article.find(params[:id])
     unless @article.access_by? current_user
